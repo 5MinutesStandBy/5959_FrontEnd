22a26184dfcd6c0f66720b43c514d8944103c0b0
Yeonwoo Ga <vjk4874@naver.com>
2022-10-24 12:04:00 +0900

    모달 레이아웃

--- src/components/modal/LoginModalSet.jsx
+++ src/components/modal/LoginModalSet.jsx
@@ -10,14 +10,69 @@ const LoginModalSet = ({closeModal}) => {
     return(
         <StModalBack>
             <StModalContainer>
-            <FontAwesomeIcon icon={faCircleXmark} size="1x" onClick={closeModal} style={{float : "right",marginTop : "3px", marginRight : "3px"}}/>
-                <p>모달창</p>
+                <StIcon>
+                <FontAwesomeIcon icon={faCircleXmark} size="1x" onClick={closeModal} style={{float : "right",marginTop : "3px", marginRight : "3px"}}/>
+                </StIcon>
+            
+                <StLoginBox>
+                    <StId>
+                        <span>아이디</span><StIdInput/>
+                    </StId>
+                    <StPass>
+                        <span>비밀번호</span><StPassInput/>
+                    </StPass>
+                    <StLoginBtn>로그인</StLoginBtn>
+                </StLoginBox>
             </StModalContainer>
         </StModalBack>
     )
 }
 
 export default LoginModalSet;
+
+const StIcon = styled.div`
+    position: absolute;
+    right:10px;
+    top:10px;
+`;
+
+const StLoginBtn = styled.button`
+    width:80px;
+    height: 25px;
+`;
+
+const StPassInput = styled.input`
+    display: flex;
+    width : 150px;
+`;
+
+const StPass = styled.div`
+    display: flex;
+    justify-content: center;
+    & span{
+        margin-right:10px;
+    }
+`;
+
+const StId = styled.div`
+    display: flex;
+    justify-content: center;
+    & span{
+        margin-right:10px;
+    }
+`;
+
+const StIdInput = styled.input`
+    width: 150px;
+    margin-left : 16px;
+`;
+
+const StLoginBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap:15px;
+    align-items: center;
+`;
 
 const StModalBack = styled.div`
     position : fixed;
@@ -40,6 +95,9 @@ const StModalContainer = styled.div`
     background-color: yellow;
     border : 1px solid black;
     border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
 `;
 
 const StButton = styled.button`
